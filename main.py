2bb6d20442f33529dba276ba301a03c195c067b3
albin-fabijan <163516249+albin-fabijan@users.noreply.github.com>
2024-03-27 11:54:51 +0100

    feature/update-main

--- main.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter import ttk
-from source.Overview import Overview
-from source.displayaddtransaction import Displayaddtransaction
-from source.DisplayLoginPage import DisplayLoginPage
-
-class main():
-    def main():
-        DisplayLoginPage().init()
