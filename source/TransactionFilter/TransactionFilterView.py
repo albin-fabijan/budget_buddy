86d75e7857ae9ad7b746fe3cb37c10c62bd60bd4
theo-baccam <theo.baccam@laplateforme.io>
2024-03-31 15:29:14 +0200

    feat(TransactionFilter): tweaked interface and filter options

--- source/TransactionFilter/TransactionFilterView.py
+++ source/TransactionFilter/TransactionFilterView.py
@@ -4,16 +4,16 @@ class TransactionFilterView(tk.Toplevel):
     def __init__(self, parent):
         self.parent = parent
         super().__init__(
-            parent
+            parent,
+            bg = "#72D5FF"
         )
         self.title("Filtre")
         self.geometry("400x400")
-        self.configure(bg='#FFFFFF')
         self.grab_set()
 
     def main(self):
-        self.sort_var = tk.StringVar(value="montant croissant")
-        self.type_var = tk.StringVar(value="ressource")
+        self.sort_var = tk.StringVar(value="Récent → Ancien")
+        self.type_var = tk.StringVar(value="...")
         self.date_var = tk.IntVar(value=0)
 
         self.create_sort()
@@ -28,87 +28,66 @@ class TransactionFilterView(tk.Toplevel):
             self,
             text="Order",
             font=("Arial", 16),
-            bg='#FFFFFF'
+            bg='#72D5FF'
         )
         sort_label.place(x=30, y=50)
 
         sort_options = [
-            "montant croissant",
-            "montant décroissant",
-            "date croissante",
-            "date décroissante"
+            "Récent → Ancien",
+            "Ancient → Récent",
+            "Montant croissant",
+            "Montant décroissant",
         ]
         self.sort_select = tk.OptionMenu(
             self,
             self.sort_var,
             *sort_options
         )
-        self.sort_select.config(font=("Arial", 16), bg='#FFFFFF', width=15)
+        self.sort_select.config(font=("Arial", 16), width=15)
         self.sort_select.place(x=150, y=50)
-
-        check_button = tk.Checkbutton(
-            self,
-            variable=self.sort_var,
-            bg='#FFFFFF'
-        )
-        check_button.place(x=375, y=55)
 
     def create_type(self):
         type_label = tk.Label(
             self,
             text="Type",
             font=("Arial", 16),
-            bg='#FFFFFF'
+            bg='#72D5FF'
         )
         type_label.place(x=30, y=150)
 
-        type_options = ["ressource", "dépense"]
+        type_options = ["...", "Revenus", "Dépenses"]
         self.type_select = tk.OptionMenu(self, self.type_var, *type_options)
-        self.type_select.config(font=("Arial", 16), bg='#FFFFFF', width=15)
+        self.type_select.config(font=("Arial", 16), width=15)
         self.type_select.place(x=150, y=150)
-
-        check_button = tk.Checkbutton(
-            self,
-            variable=self.type_var,
-            bg='#FFFFFF'
-        )
-        check_button.place(x=375, y=155)
         
     def create_date(self):
         date_label = tk.Label(
             self,
             text="Date :",
             font=("Arial", 16),
-            bg='#FFFFFF'
+            bg='#72D5FF'
         )
         date_label.place(x=30, y=250)
 
-        de_label = tk.Label(self, text="De", font=("Arial", 14), bg='#FFFFFF')
+        de_label = tk.Label(self, text="De", font=("Arial", 14), bg='#72D5FF')
         de_label.place(x=120, y=250)
 
         self.date_minentry = tk.Entry(self, font=("Arial", 16), width=7)
         self.date_minentry.place(x=150, y=250)
 
-        a_label = tk.Label(self, text="à", font=("Arial", 14), bg='#FFFFFF')
+        a_label = tk.Label(self, text="à", font=("Arial", 14), bg='#72D5FF')
         a_label.place(x=230, y=250)
 
         self.date_maxentry = tk.Entry(self, font=("Arial", 16), width=7)
         self.date_maxentry.place(x=250, y=250)
 
-        check_button = tk.Checkbutton(
-            self,
-            variable=self.date_var,
-            bg='#FFFFFF'
-        )
-        check_button.place(x=375, y=255)
-
     def create_erase_button(self):
-        eraise_button = tk.Button(
+        erase_button = tk.Button(
             self,
             text="Effacer",
             font=("Arial", 16),
         )
-        eraise_button.place(x=250, y=350)
+        erase_button.place(x=250, y=350)
 
     def create_apply_button(self):
         apply_button = tk.Button(
